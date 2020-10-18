b1a0447a3ba89af19829e4a432f25e172b8af254
gegese <arcmhmt@gmail.com>
2020-10-18 15:31:03 +0300

    Added sidebar and navbar

---
+++ components/sidebar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>DigaPanel V.1.0</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="includes/css/sidebar.css" rel="stylesheet">
+  <link rel="icon" type="image/png" href="assets/digapanel-favicon.png"/>
+</head>
+
+<body>
+
+  <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading">DigaPanel V.1.0 </div>
+      <div class="list-group list-group-flush mt-5">
+        <a href="#" class="list-group-item list-group-item-action bg-light">Sayfalar</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Seo Optimizasyonu</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Ayarlar</a>
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	include 'components/sidebar.php';
+	include 'components/navbar.php'; 
+?>
+
+<center>
+	<h1 class="mt-5">PANELİ KULLANAN FİRMANIN ADI</h1>
+	<h4 class="mt-2">Kontrol Paneli</h4>
+</center>
+
+<?php
+    include 'components/footer.php';
+?>
